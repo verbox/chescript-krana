57a51c466d63ab27a010a90bd4c5ba932eda01ce
Piotr J <poap@interia.pl>
2024-10-12 21:16:55 +0200

    init modules

---
+++ code/krana-parent/krana-server/src/main/java/org/krana/KranaServerApplication.java
@@ -0,0 +1,13 @@
+package org.krana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KranaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KranaServerApplication.class, args);
+	}
+
+}
---
+++ code/krana-parent/krana-server/src/test/java/org/krana/KranaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.krana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KranaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
